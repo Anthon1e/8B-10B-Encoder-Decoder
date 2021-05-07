1251892088940fbbf896686eb741054001260df8
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-05-07 16:14:28 +0700

    Need to fix timing

--- disCtrl.v
+++ disCtrl.v
@@ -27,9 +27,10 @@ module disCtrl(
     input [4:0] data_buffer,
     input D, 
     input E,
-    output reg PDL6_upd,
-    output COMPLS6,
-    output COMPLS4
+    input rd_in,
+    output reg COMPLS6,
+    output reg COMPLS4,
+    output reg rd_out
     );
     wire L40, L31, L22, L13, L04, K, K4, F4, G4, H4, S;
     assign {S, K4, H4, G4, F4} = data_buffer;
@@ -39,45 +40,47 @@ module disCtrl(
         D1 stands for D-1 (entry running disparity)
         D0 stands for D0  (current running disparity) */
     /* Disparity classification for 5B/6B */
-    wire PD1S6, ND0S6, ND1S6, PD0S6;
+    // If either PD1S6 or ND1S6 is set, then RD6 is changed 
+    wire PD1S6, ND0S6, ND1S6, PD0S6, RD6;
     assign PD1S6 = !((L13 & D & E) ^ (~L22 & ~L31 & ~E));
     assign ND0S6 = PD1S6;
     assign ND1S6 = (L31 & ~D & ~E) | (E & ~L22 & ~L13) | K;
     assign PD0S6 = (E & ~L22 & ~L13) | K;
+    assign RD6 = (PD0S6 | ND0S6);  
     /* Disparity classification for 3B/4B */  
-    wire ND1S4, ND0S4, PD1S4, PD0S4; 
+    // If either PD1S5 or ND1S4 is set, then RD4 is changed
+    wire ND1S4, ND0S4, PD1S4, PD0S4, RD4; 
     assign ND1S4 = F4 & G4; 
     assign ND0S4 = ~F4 & ~G4; 
     assign PD1S4 = ND0S4 | ((F4^G4) & K4);
     assign PD0S4 = F4 & G4 & H4;
-    /* Control of complementation:
-        NEED MORE CLARIFICATION ON THIS
-    */
-    wire PDL4, PDL6;
-    reg PDL4_upd; 
-    /* Upper flip-flop for running disparity of bit i */
-    assign PDL6 = (PD0S6 & ~COMPLS6) |
-                  (COMPLS6 & ND0S6)  |
-                  (~ND0S6 & ~PD0S6 & ~PDL4_upd);    // Current running disparity for bit i
+    assign RD4 = (PD0S4 | ND0S4);
+    // Assign rd_out for the next input
+    reg rd_cur;
     always @(posedge clk)
-    begin
-        if (reset)
-            PDL6_upd <= 0;
-        else
-            PDL6_upd <= PDL6; 
-    end
-    // Compare with D-1 entry disparity to determine the complement for 4B
-    assign COMPLS4 = (ND1S4 & PDL6_upd)^(~PDL6_upd & PD1S4);
-    /* Lower flip-flop for running disparity of bit j */
-                                                    // Current running disparity for bit j
-    assign PDL4 = (~COMPLS4 & PD0S4)^(ND0S4 & COMPLS4)^(~PDL6_upd & ~PD0S4 & ~ND0S4);                     
-    always @(negedge clk)
     begin 
-        if (reset)
-            PDL4_upd <= 0;
-        else
-            PDL4_upd <= PDL4; 
-    end 
-    // Compare with D-1 entry disparity to determine the complement for 6B
-    assign COMPLS6 = (ND1S6 & PDL4_upd)^(~PDL4_upd & PD1S6);
+        rd_cur = RD6 ^ RD4;         // Running disparity of current input
+        rd_out = rd_cur ^ rd_in;    // Use running disparity of current input to determine if we
+                                    //      need to change entry running disparity of the next input.
+    end
+    /* Control of complementation:
+        The complement is set if rd_in's sign does not 
+        matched with D1S6 and D1S4 sign                 */
+    // Disparity of D0S6 is based on the entry running disparity
+    // Complement is set when PD1S6 is 1 (expect positive), but rd_in is 0(-) 
+    //                     OR ND1S6 is 1 (expect negative), but rd_in is 1(+)
+    always @(posedge clk)
+    begin 
+        COMPLS6 <= (PD1S6 & ~rd_in) | (ND1S6 & rd_in);
+    end
+    // PD1S4 is same, but here, the entry disparity 
+    //      will be the out disparity of fcn 5B 
+    // Complement is set when PD1S4 is 1 (expect positive), but rd_in is 0(-)
+    //                     OR ND1S4 is 1 (expect negative), but rd_in is 1(+)
+    reg rd_in_s4;
+    always @(negedge clk)
+    begin
+        rd_in_s4 = RD6 ^ rd_in;
+        COMPLS4 = ((PD1S4 & ~rd_in_s4) | (ND1S4 & rd_in_s4));
+    end
 endmodule
