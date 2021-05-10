a8fc2ae59a067020057f7a08b688c9aa8e79cc8d
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-05-10 16:24:42 +0700

    All cases work, but need fix on timing constraint

--- Enc8B10B/Enc8B10B.srcs/sources_1/new/disCtrl.v
+++ Enc8B10B/Enc8B10B.srcs/sources_1/new/disCtrl.v
@@ -28,6 +28,7 @@ module disCtrl(
     input D, 
     input E,
     input rd_in,
+    output reg rd_in_s4,
     output reg COMPLS6,
     output reg COMPLS4,
     output reg rd_out
@@ -42,7 +43,7 @@ module disCtrl(
     /* Disparity classification for 5B/6B */
     // If either PD1S6 or ND1S6 is set, then RD6 is changed 
     wire PD1S6, ND0S6, ND1S6, PD0S6, RD6;
-    assign PD1S6 = !((L13 & D & E) ^ (~L22 & ~L31 & ~E));
+    assign PD1S6 = (L13 & D & E) ^ (~L22 & ~L31 & ~E);
     assign ND0S6 = PD1S6;
     assign ND1S6 = (L31 & ~D & ~E) | (E & ~L22 & ~L13) | K;
     assign PD0S6 = (E & ~L22 & ~L13) | K;
@@ -57,11 +58,11 @@ module disCtrl(
     assign RD4 = (PD0S4 | ND0S4);
     // Assign rd_out for the next input
     reg rd_cur;
-    always @(posedge clk)
+    always @(*)
     begin 
         rd_cur = RD6 ^ RD4;         // Running disparity of current input
         rd_out = rd_cur ^ rd_in;    // Use running disparity of current input to determine if we
-                                    //      need to change entry running disparity of the next input.
+                                    //  need to change entry running disparity of the next input.
     end
     /* Control of complementation:
         The complement is set if rd_in's sign does not 
@@ -69,16 +70,15 @@ module disCtrl(
     // Disparity of D0S6 is based on the entry running disparity
     // Complement is set when PD1S6 is 1 (expect positive), but rd_in is 0(-) 
     //                     OR ND1S6 is 1 (expect negative), but rd_in is 1(+)
-    always @(posedge clk)
+    always @(*)
     begin 
         COMPLS6 <= (PD1S6 & ~rd_in) | (ND1S6 & rd_in);
     end
     // PD1S4 is same, but here, the entry disparity 
-    //      will be the out disparity of fcn 5B 
+    //  will be the out disparity of fcn 5B 
     // Complement is set when PD1S4 is 1 (expect positive), but rd_in is 0(-)
     //                     OR ND1S4 is 1 (expect negative), but rd_in is 1(+)
-    reg rd_in_s4;
-    always @(negedge clk)
+    always @(*)
     begin
         rd_in_s4 = RD6 ^ rd_in;
         COMPLS4 = ((PD1S4 & ~rd_in_s4) | (ND1S4 & rd_in_s4));
