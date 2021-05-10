a7d18358ffc3a56baebadd8e7ac2d906f4bd2f2d
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-05-10 16:24:19 +0700

    All cases work, but need fix on timing constraint

--- Enc8B10B/Enc8B10B.srcs/sources_1/new/Enc8B10B.v
+++ Enc8B10B/Enc8B10B.srcs/sources_1/new/Enc8B10B.v
@@ -25,12 +25,13 @@ module Enc8B10B(
     input reset,
     input bit_control,
     input [7:0] in,
-    output reg [9:0] out
+    output reg [9:0] out,
+    output rd_out
     );
     
     // Variable to hold values 
     wire clk = BYTECLK; 
-    wire PDL6, COMPLS6, COMPLS4;
+    wire rd_in_s4, COMPLS6, COMPLS4;
     wire [5:0] L;    // L
     wire [4:0] data_buffer;  
     wire [5:0] abcdei;
@@ -38,38 +39,31 @@ module Enc8B10B(
     
     reg [7:0] data_in;
     reg K, rd_in, en;
-    wire next_K, rd_out;
-    
-    always @(posedge clk) 
-    begin 
-        if (reset) begin
-            K <= 0;
-        end
-        else begin
-            K <= bit_control;
-        end
-    end
+    wire next_K;
     
     always @(negedge clk) 
     begin 
         if (reset) begin
             data_in <= 8'b0;
             rd_in <= 0;
+            K <= 0;
         end
         else begin
             rd_in <= rd_out;
             data_in <= in;
+            K <= bit_control;
         end
     end
     
     fcn5b   f5b(clk, reset, K, data_in[4:0], L);
-    fcn3b   f3b(clk, reset, K, data_in[7:3], PDL6, L, data_buffer); 
-    disCtrl dis(clk, reset, L, data_buffer, data_in[4], data_in[3], rd_in, COMPLS6, COMPLS4, rd_out);
+    fcn3b   f3b(clk, reset, K, data_in[7:3], rd_in_s4, L, data_buffer); 
+    disCtrl dis(clk, reset, L, data_buffer, data_in[3], data_in[4], 
+                rd_in, rd_in_s4, COMPLS6, COMPLS4, rd_out);
     fcn5b6b f56(clk, reset, data_in[4:0], L, COMPLS6, abcdei);
     fcn3b4b f34(clk, reset, data_buffer, COMPLS4, fghj);
     
     always @(negedge clk)
     begin
-        out = {abcdei[5:0], fghj[3:0]}; // Encoded messages
+        out <= {abcdei[5:0], fghj[3:0]}; // Encoded messages
     end
 endmodule
