623928e6f64ba0449ac097d245df3b7e3ed1081b
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-05-11 16:39:41 +0700

    Fix timing constraints, need to fix post-synthesis

--- Enc8B10B/Enc8B10B.srcs/sources_1/new/Enc8B10B.v
+++ Enc8B10B/Enc8B10B.srcs/sources_1/new/Enc8B10B.v
@@ -25,45 +25,40 @@ module Enc8B10B(
     input reset,
     input bit_control,
     input [7:0] in,
-    output reg [9:0] out,
+    output [9:0] out,
     output rd_out
     );
     
     // Variable to hold values 
     wire clk = BYTECLK; 
-    wire rd_in_s4, COMPLS6, COMPLS4;
-    wire [5:0] L;    // L
-    wire [4:0] data_buffer;  
+    wire rd_in_s4, S, COMPLS6, COMPLS4, saved_K;
+    wire [4:0] L, saved_L;    
+    wire [7:0] saved_data_in;
     wire [5:0] abcdei;
     wire [3:0] fghj; 
     
     reg [7:0] data_in;
-    reg K, rd_in, en;
-    wire next_K;
+    reg K, rd_in;
     
-    always @(negedge clk) 
-    begin 
-        if (reset) begin
-            data_in <= 8'b0;
-            rd_in <= 0;
-            K <= 0;
-        end
-        else begin
-            rd_in <= rd_out;
-            data_in <= in;
-            K <= bit_control;
-        end
-    end
+    fcn5b   f5b(clk, data_in[4:0], L);
+    fcn3b   f3b(clk, data_in[4:3], rd_in_s4, L, S); 
+    disCtrl dis(clk, reset, K, L, S, data_in, rd_in, saved_data_in, saved_L, saved_K, rd_in_s4, COMPLS6, COMPLS4, rd_out);
+    fcn5b6b f56(clk, reset, saved_data_in[4:0], saved_L, saved_K, COMPLS6, abcdei);
+    fcn3b4b f34(clk, reset, saved_data_in[7:5], S, saved_K, COMPLS4, fghj);
     
-    fcn5b   f5b(clk, reset, K, data_in[4:0], L);
-    fcn3b   f3b(clk, reset, K, data_in[7:3], rd_in_s4, L, data_buffer); 
-    disCtrl dis(clk, reset, L, data_buffer, data_in[3], data_in[4], 
-                rd_in, rd_in_s4, COMPLS6, COMPLS4, rd_out);
-    fcn5b6b f56(clk, reset, data_in[4:0], L, COMPLS6, abcdei);
-    fcn3b4b f34(clk, reset, data_buffer, COMPLS4, fghj);
-    
-    always @(negedge clk)
+    always @(posedge clk, posedge reset)
     begin
-        out <= {abcdei[5:0], fghj[3:0]}; // Encoded messages
+        if (reset) begin 
+            K <= 0;
+            rd_in = 0;
+            data_in = 0;
+        end
+        else begin 
+            K <= bit_control;
+            rd_in = rd_out;
+            data_in = in;
+        end
     end
+    
+    assign out = {abcdei[5:0], fghj[3:0]}; // Encoded messages
 endmodule
