f062c2ae01c3e26309166367ba8c85fe3ca60de8
Anthon1e <57845986+Anthon1e@users.noreply.github.com>
2021-05-14 14:29:04 +0700

    Post-synthesis functional simulation is working

--- Dec8B10B/Dec8B10B_tb.v
+++ Dec8B10B/Dec8B10B_tb.v
@@ -61,37 +61,38 @@ module Dec8B10B_tb();
             j = j + 2;
         end
         err = 0;
-        #2; 
+        /* Wait time for FPGA configuration */
+        #120; 
         
         /* Begin the reset stage */
         reset = 1; 
         #2; 
         
-        /* Give the decoder 2^10 inputs, from 0 to 1023*/
+        /* Give the decoder 2^10 inputs, from 0 to 1023 */
         for (i = 0; i < 1024; i = i + 1) begin
             /* Reset stage */
             if (reset == 1) begin
                 in = 0; 
                 reset = 0;
+                #4;
                 invalid_count = 0;
-                valid_count = 0;
-                #2; 
+                valid_count = 0; 
             /* Begin with all 268 cases */ 
             end else begin  
                 /* If the input is raised as invalid, check with the file if it actually should be */
                 if (code_err == 1) begin 
-                    invalid_count <= invalid_count + 1;     // Count number of invalid inputs
+                    invalid_count = invalid_count + 1;     // Count number of invalid inputs
                     for (j = 0; j < 536; j = j + 1) begin
-                        if ((in - 1) == value_in[j]) check = 1;
+                        if ((in - 2) == value_in[j]) check = 1;
                     end
                     if (check == 32'b1) err = 1;
                     check = 0;
-                    end
+                end
                 /* If the input is valid, check with the file if it actually should be */
                 if (code_err == 0) begin 
-                    valid_count <= valid_count + 1;         // Count number of valid inputs
+                    valid_count = valid_count + 1;         // Count number of valid inputs
                     for (j = 0; j < 536; j = j + 1) begin
-                        if ((in - 1) == value_in[j]) check = 1;
+                        if ((in - 2) == value_in[j]) check = 1;
                     end
                     if (check == 32'b0) err = 1;
                     check = 0;
@@ -103,10 +104,14 @@ module Dec8B10B_tb();
             end
         end
         
-        /* Final check for the last input, this is because there is 1 cycle delay */
+        /* Second last check for the second last input, this is because there is a 2 cycle delay */
         if (code_err == 1)  invalid_count = invalid_count + 1; 
         if (code_err == 0)  valid_count = valid_count + 1;
+        in = 10'bxxxxxxxxxx;
         #2;
+        /* Final check for the last input, this is because there is a 2 cycle delay */
+        if (code_err == 1)  invalid_count = invalid_count + 1; 
+        if (code_err == 0)  valid_count = valid_count + 1;
         
         /* If no error, code is working */
         if (~err) $display("INTERFACE OK");
